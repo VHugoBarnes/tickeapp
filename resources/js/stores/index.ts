3eb4349552883ef95f1186c8a5818a8f5f403749
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-24 14:50:29 -0600

    pinia store

--- resources/js/stores/index.ts
+++ resources/js/stores/index.ts
@@ -1,2 +1,41 @@
 import { defineStore } from "pinia";
 
+export enum Tab {
+  event = "event",
+  hotel = "hotel"
+}
+
+export interface AutoCompleteResults { }
+export interface Event { }
+export interface Hotel { }
+
+interface State {
+  loading: boolean;
+  tab: Tab;
+  autocompleteResults: AutoCompleteResults[],
+  results: Event[] | Hotel[]
+}
+
+export const useCounterStore = defineStore("results", {
+  state: (): State => ({
+    loading: false,
+    tab: Tab.event,
+    autocompleteResults: [],
+    results: [],
+
+  }),
+  actions: {
+    setLoading(value: boolean) {
+      this.loading = value;
+    },
+    setTab(tab: Tab) {
+      this.tab = tab;
+    },
+    setAutoCompleteResults(options: AutoCompleteResults[]) {
+      this.autocompleteResults = options;
+    },
+    setResults(results: Event[] | Hotel[]) {
+      this.results = results;
+    }
+  }
+});
