e6e19d177f29bb1836b229cb47b1b93c84ba3f06
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-24 21:16:02 -0600

    Update Tabs

--- resources/js/Components/Tabs.vue
+++ resources/js/Components/Tabs.vue
@@ -2,14 +2,14 @@
   <div class="flex items-center space-x-2 text-lg">
     <button
       class="text-gray-300 bg-gray-700 hover:bg-gray-900 py-2 px-4 rounded-lg"
-      :class="{ 'bg-gray-900': resultsStore.tab === Tab.event }"
+      :class="{ 'bg-gray-900 border-solid border-t-2 border-rose-700 text-rose-700': resultsStore.tab === Tab.event }"
       @click="resultsStore.setTab(Tab.event)"
     >
       Event
     </button>
     <button
       class="text-gray-300 bg-gray-700 hover:bg-gray-900 py-2 px-4 rounded-lg"
-      :class="{ 'bg-gray-900': resultsStore.tab === Tab.hotel }"
+      :class="{ 'bg-gray-900 border-solid border-t-2 border-rose-700 text-rose-700': resultsStore.tab === Tab.hotel }"
       @click="resultsStore.setTab(Tab.hotel)"
     >
       Hotel
